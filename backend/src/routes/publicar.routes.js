4f165d47b9b68ef92bbecbd8b90f0212390bbe79
DaniRub04 <rubiocamacho297@gmail.com>
2026-02-11 12:34:45 -0600

    fix(publicar): correct controller filename to publicar.controllers.js

--- backend/src/routes/publicar.routes.js
+++ backend/src/routes/publicar.routes.js
@@ -1,6 +1,6 @@
 // backend/src/routes/publicar.routes.js
 import { Router } from "express";
-import { publicar } from "../controllers/publicar.controller.js";
+import { publicar } from "../controllers/publicar.controllers.js";
 import { auth } from "../middlewares/auth.js";
 
 const router = Router();
